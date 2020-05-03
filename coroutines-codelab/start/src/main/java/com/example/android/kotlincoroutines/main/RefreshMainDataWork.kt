df671845e0ec139fd3a24f09ee38a37670c101eb
Tim Davis <timdavis10310@gmail.com>
2020-05-03 18:17:40 +0800

    ch 12
    * test coroutines with workerManager by ListenableWorker

--- coroutines-codelab/start/src/main/java/com/example/android/kotlincoroutines/main/RefreshMainDataWork.kt
+++ coroutines-codelab/start/src/main/java/com/example/android/kotlincoroutines/main/RefreshMainDataWork.kt
@@ -39,13 +39,27 @@ class RefreshMainDataWork(context: Context, params: WorkerParameters, private va
      * WorkManager will call this method from a background thread. It may be called even
      * after our app has been terminated by the operating system, in which case [WorkManager] will
      * start just enough to run this [Worker].
+     *
+     * this code does NOT run on the Executor specified in your WorkManager configuration,
+     * but instead use the dispatcher in coroutineContext member (by default Dispatchers.Default).
      */
     override suspend fun doWork(): Result {
-        return Result.success()         // TODO: Use coroutines from WorkManager
+        val database = getDatabase(applicationContext)
+        val repository = TitleRepository(network, database.titleDao)
+        return try {
+            repository.refreshTitle()
+            Result.success()
+        } catch (error: TitleRefreshError) {
+            Result.failure()
+        }
     }
 
-    class Factory(val network: MainNetwork = getNetworkService()) : WorkerFactory() {
-        override fun createWorker(appContext: Context, workerClassName: String, workerParameters: WorkerParameters): ListenableWorker? {
+    class Factory(private val network: MainNetwork = getNetworkService()) : WorkerFactory() {
+        override fun createWorker(
+                appContext: Context,
+                workerClassName: String,
+                workerParameters: WorkerParameters)
+                : ListenableWorker? {
             return RefreshMainDataWork(appContext, workerParameters, network)
         }
 
